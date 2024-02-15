6625c1540abf429a85657daa8383804e88530ef6
Hailo7ts <101282320+Hailo7ts@users.noreply.github.com>
2024-02-15 13:07:54 -0700

    update img location

--- scrimba-p1/src/components/Section.jsx
+++ scrimba-p1/src/components/Section.jsx
@@ -1,7 +1,7 @@
 export default function Section(){
     return(
         <section className="section-container">
-            <img src="./src/qr.png"/>
+            <img src="../images/qr.png"/>
             <h1>Hailey Martin</h1>
             <h2>Software Engineer</h2>
         </section>
