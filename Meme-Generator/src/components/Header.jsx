29b3e9f1ca1dbfd1c142457c7949d1b460ecd156
Hailo7ts <101282320+Hailo7ts@users.noreply.github.com>
2024-02-20 15:08:01 -0700

    Project start
    React responsive web app start

---
+++ Meme-Generator/src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header(){
+    return(
+        <header className="header-container">
+            <img src="..\img\Troll Face.png" alt="logo" />
+            <h2>Meme Generator</h2>
+            <h4 className="header-project-title">React Course - Project 3</h4>
+        </header>
+    )
+    
+}
